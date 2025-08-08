3b85a6778d85b486f926a1d0fbfdcc7ccccd633a
Sheinrei <l.beaute@laposte.net>
2025-08-08 09:08:12 +0200

    fix url brok

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dsn' => "mysql:host=localhost;dbname=portfolio",
+    'user' => "root",
+    'password' => "",
+    'mailPassword' => 'nizb gdzj dmkj ttns'
+];
+
+
+// *ET7*zkSu,N@ DBLaurent
